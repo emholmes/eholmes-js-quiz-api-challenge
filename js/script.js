eea6cc005be06e30541b178b86453fbfc330f1ac
Erin Holmes <e.holmes09@yahoo.com>
2021-06-02 22:13:54 -0500

    first pass at saving and loading scores

--- js/script.js
+++ js/script.js
@@ -1,42 +1,35 @@
+var questionsObject = [
+    { q: "What's 1 + 1?", a: "2", choice1: "4", choice2: "1", choice3: "2", choice4: "0" }, 
+    { q: "What's 1 + 2?", a: "3", choice1: "5", choice2: "3", choice3: "8", choice4: "9" }, 
+    { q: "What Tobi's nickname?", a: "all of the above", choice1: "Toblerone", choice2: "Tobes", choice3: "Tobias", choice4: "all of the above" }
+]
+
 var headerSection = document.querySelector("header");
 var heroSection = document.getElementById("hero");
 var quizSection = document.getElementById("quiz");
 var quizResultsSection = document.getElementById("quiz-results");
 var highScoresSection = document.getElementById("high-scores");
-
-var questionsObject = [
-    {
-        q: "What's 1 + 1?", 
-        a: "2", 
-        choice1: "4",
-        choice2: "1",
-        choice3: "2",
-        choice4: "0"
-    }, 
-    {
-        q: "What's 1 + 2?", 
-        a: "3",
-        choice1: "5",
-        choice2: "3",
-        choice3: "8",
-        choice4: "9"
-    }, 
-    {
-        q: "What Tobi's nickname?", 
-        a: "all of the above",
-        choice1: "Toblerone",
-        choice2: "Tobes",
-        choice3: "Tobias",
-        choice4: "all of the above"
-    }
-]
-
-
+var scoreForm = document.getElementById("score-form");
 
 var qIndex = 0;
 var correct;
 var points = 0;
 var player = [];
+
+var scoreFormHandler = function(event) {
+    event.preventDefault();
+
+    var playerScore = points;
+    var playerName = document.querySelector("input[name='player-initials']").value;
+
+    var scoreObj = {
+        score: playerScore,
+        name: playerName
+    };
+    createScoreEl(scoreObj);
+
+    saveScore();
+}
 
 var getQuestion = function() {
     if (qIndex < questionsObject.length) {
@@ -80,8 +73,18 @@ var getQuestion = function() {
     } else {
         quizResultsSection.style.display = "block";
         document.getElementById("final-score").textContent = "Your final score is " + points;
-        //saveScore();
     }
+}
+
+var createScoreEl = function(scoreObj) {
+    var listItem = document.createElement("li");
+    listItem.classname = "score";
+    //add code to enter score details - use score Obj
+    listItem.textContent = scoreObj.name;
+    var ulElement = document.getElementById("scores-list");
+    ulElement.appendChild(listItem);
+    player.push(scoreObj);
+
 }
 
 // view high scores link
@@ -91,6 +94,8 @@ document.getElementById("view-scores").addEventListener("click", function() {
     quizSection.style.display = "none";
     quizResultsSection.style.display = "none";
     highScoresSection.style.display = "block";
+
+    loadScores();
 });
 
 //go back button
@@ -104,12 +109,26 @@ document.getElementById("back").addEventListener("click", function(){
 
 
 var saveScore = function() {
-    localStorage.setItem("high score", points);
+    localStorage.setItem("scores", JSON.stringify(player));
+}
+
+var loadScores = function () {
+    var savedScores = localStorage.getItem("scores");
+    if (!savedScores) {
+        return false;
+    } 
+    savedScores = JSON.parse(savedScores);
+
+    for (var i = 0; i < savedScores.length; i++) {
+        createScoreEl(savedScores[i]);
+    }
 }
 
 var startQuiz = function() {
     heroSection.style.display = "none";
     getQuestion();
 }
+
+scoreForm.addEventListener("submit", scoreFormHandler);
 
 document.getElementById("startQuizBtn").addEventListener("click", startQuiz);
