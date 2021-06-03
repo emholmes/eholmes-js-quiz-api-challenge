fefd969b5b85bc15bf7765c1fdb6793c7c2fa9dc
Erin Holmes <e.holmes09@yahoo.com>
2021-06-02 19:49:19 -0500

    refactor html and js to not have js create all html elements

--- js/script.js
+++ js/script.js
@@ -27,19 +27,6 @@ var questionsObject = [
 
 var intro = document.getElementById("intro-content");
 
-var introHeader = document.createElement("h1");
-introHeader.textContent = "Javascript Code Quiz";
-intro.appendChild(introHeader);
-
-var introCopy = document.createElement("p");
-introCopy.textContent = "Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by 10 seconds!";
-intro.appendChild(introCopy);
-
-var startQuizBtn = document.createElement("button");
-startQuizBtn.setAttribute("id", "startQuizBtn");
-startQuizBtn.textContent = "Start Quiz";
-intro.appendChild(startQuizBtn);
-
 var qIndex = 0;
 var correct;
 var points = 0;
@@ -86,32 +73,12 @@ var getQuestion = function() {
             });
         }
     } else {
-        saveScore();
+        
         var quizComplete = document.getElementById("quizComplete");
-        var allDone = document.createElement("h2");
-        allDone.textContent = "All done!";
-        var finalScoreReport = document.createElement("p");
+        quizComplete.style.display = "block";
+        var finalScoreReport = document.getElementById("final-score");
         finalScoreReport.textContent = "Your final score is " + points;
-        quizComplete.appendChild(allDone);
-        quizComplete.appendChild(finalScoreReport);
-        // form to enter name
-        var entryForm = document.createElement("form");
-        var entryFormLabel = document.createElement("label");
-        entryFormLabel.setAttribute("for", "player-name");
-        var entryFormInput = document.createElement("input");
-        entryFormInput.setAttribute("type", "text");
-        entryFormInput.setAttribute("id", "player-name");
-        entryFormInput.setAttribute("name", "player-name");
-        var entryFormSubmit = document.createElement("button");
-        entryFormSubmit.className = "btn btn-dark";
-        entryFormSubmit.setAttribute("type", "submit");
-        entryFormSubmit.setAttribute("value", "submit");
-        entryFormSubmit.textContent = "Submit";
-        quizComplete.appendChild(entryForm);
-        entryForm.appendChild(entryFormLabel);
-        entryForm.appendChild(entryFormInput);
-        entryForm.appendChild(entryFormSubmit);
-
+        saveScore();
     }
 }
 
